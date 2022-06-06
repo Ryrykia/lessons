3d31c00a20b90e4fad06a417f4b0065fe2311585
Kochneva Kristina <103591010+Ryrykia@users.noreply.github.com>
2022-06-06 15:02:29 +0500

    Add files via upload

---
+++ Пароль/проверка пароля/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace проверка_пароля
+{
+    partial class Password
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Ok = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите пароль";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.Location = new System.Drawing.Point(38, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 3;
+            // 
+            // Ok
+            // 
+            this.Ok.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Ok.Location = new System.Drawing.Point(244, 68);
+            this.Ok.Name = "Ok";
+            this.Ok.Size = new System.Drawing.Size(127, 31);
+            this.Ok.TabIndex = 4;
+            this.Ok.Text = "Готово";
+            this.Ok.UseVisualStyleBackColor = false;
+            this.Ok.Click += new System.EventHandler(this.Ok_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label3.Location = new System.Drawing.Point(38, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.Location = new System.Drawing.Point(38, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5.Location = new System.Drawing.Point(38, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 31);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // Password
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(496, 277);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Ok);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Ok;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Пароль/проверка пароля/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+namespace проверка_пароля
+{
+    public partial class Password : Form
+    {
+        public Password()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Ok_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text.Length < 6)
+             {
+                label2.Text = "Пароль должен содержать минимум 6 символов";
+             }
+            
+            if (textBox1.Text.Any(c => char.IsLower(c)))
+            {
+                label3.Text = " ";
+            }
+            else
+            {                
+                label3.Text = "Пароль должен содержать минимум 1 пропусную букву";
+            }
+            if (textBox1.Text.Any(c => char.IsDigit(c)))
+            {
+                label4.Text = " ";
+            }
+            else
+            {
+                label4.Text = "Пароль должен содержать минимум 1 цифру";
+            }
+            if (textBox1.Text.Any(c => char.IsPunctuation(c)))
+            {
+                label5.Text = " ";
+            }
+            else
+            {              
+                label5.Text = "Пароль должен содержать минимум 1 из специальных символов ! @ # $ % ^";
+            }
+            if (textBox1.Text.Length >= 6 && textBox1.Text.Any(c => char.IsLower(c))&& textBox1.Text.Any(c => char.IsDigit(c))&& textBox1.Text.Any(c => char.IsPunctuation(c)))
+            {               
+                FileStream file = new FileStream(@"F:text.txt", FileMode.OpenOrCreate);
+                StreamWriter writer = new StreamWriter(file);
+                writer.WriteLine(textBox1.Text);
+                writer.Close();
+                label6.Text = "Пароль успешно сохранен";
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+     
